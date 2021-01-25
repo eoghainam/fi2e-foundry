e78846a869ec3571e2b876dc6a74e3a822a20556
Eoghain <eoghainam@users.noreply.github.com>
2021-01-25 21:25:56 +0000

    Update FI2ENamedCharacterSheet.js

--- module/sheets/FI2ENamedCharacterSheet.js
+++ module/sheets/FI2ENamedCharacterSheet.js
@@ -1,7 +1,7 @@
 export default class FI2ENamedCharacterSheet extends ActorSheet {
     static get defaultOptions(){
         return mergeObject(super.defaultOptions, {
-            template: "systems/fi2e/templates/sheets/FI2ENamedCharacterSheet.hbs",
+            template: "./fi2e/templates/sheets/FI2ENamedCharacterSheet.hbs",
             classes: ["fi2e", "sheet", "namedCharacter"]
         });
     }
