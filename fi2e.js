bb8cb177f062ce83141d3a18f48a5e72cbdd1bb1
Eoghain <eoghainam@users.noreply.github.com>
2021-01-25 22:11:41 +0000

    Update fi2e.js

--- fi2e.js
+++ fi2e.js
@@ -4,7 +4,7 @@ import FI2ENamedCharacterSheet from "./module/sheets/FI2ENamedCharacterSheet.js"
 Hooks.once("init", function(){
     console.log("fi2e | initialising Fear Itself 2nd edition system");
 
-    CONFIG.fi2e = fi2e;
+    //CONFIG.fi2e = fi2e;
 
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("fi2e", FI2ENamedCharacterSheet, {makeDefault: true});
