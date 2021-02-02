b9bd0534fda643da794b229ff44816799a70385c
Eoghain <eoghainam@users.noreply.github.com>
2021-02-02 23:59:45 +0000

    use string interpolation to break up sheets

--- module/sheets/FI2EItemSheet.js
+++ module/sheets/FI2EItemSheet.js
@@ -1,8 +1,15 @@
 export default class FI2EItemSheet extends ItemSheet {
+    
+    get template(){
+        return `system/fi2e/templates/sheets/${this.item.data.type}-sheet.html`; 
+    }
+    
+    /*
     static get defaultOptions(){
         return mergeObject(super.defaultOptions, {
             template: "systems/fi2e-foundry/templates/sheets/FI2EItemSheet.hbs",
             classes: ["fi2e", "sheet", "item"]
         });
     }
+    */
 }
