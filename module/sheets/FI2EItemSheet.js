bb4c4a16567b98e395f94e45d505a8fc5079983a
Eoghain <eoghainam@users.noreply.github.com>
2021-02-03 01:12:35 +0000

    fixed typo
    typed html instead of hbs

--- module/sheets/FI2EItemSheet.js
+++ module/sheets/FI2EItemSheet.js
@@ -1,7 +1,7 @@
 export default class FI2EItemSheet extends ItemSheet {
     
     get template(){
-        return `systems/fi2e-foundry/templates/sheets/${this.item.data.type}-sheet.html`; 
+        return `systems/fi2e-foundry/templates/sheets/${this.item.data.type}-sheet.hbs`; 
     }
     
     /*
