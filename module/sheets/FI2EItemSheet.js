e24ae16650161acc26fe339a549b930cad380de1
Eoghain <eoghainam@users.noreply.github.com>
2021-02-03 00:26:15 +0000

    trying to fix string interpolation

--- module/sheets/FI2EItemSheet.js
+++ module/sheets/FI2EItemSheet.js
@@ -1,15 +1,16 @@
 export default class FI2EItemSheet extends ItemSheet {
-    /* 
+    
     get template(){
         return `system/fi2e/templates/sheets/${this.item.data.type}-sheet.html`; 
     }
-    */
     
+    /*
     static get defaultOptions(){
         return mergeObject(super.defaultOptions, {
-            template: `systems/fi2e-foundry/templates/sheets/${this.item.data.type}-sheet.hbs`,
+            template: `systems/fi2e-foundry/templates/sheets/${this.item.data.types}-sheet.hbs`,
             classes: ["fi2e", "sheet", "item"]
         });
     }
+    */
     
 }
