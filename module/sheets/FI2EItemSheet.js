5bdd847cba5aab9859f4f49c74fba3bb62d14998
Eoghain <eoghainam@users.noreply.github.com>
2021-03-01 14:44:20 +0000

    remove test code

--- module/sheets/FI2EItemSheet.js
+++ module/sheets/FI2EItemSheet.js
@@ -2,15 +2,5 @@ export default class FI2EItemSheet extends ItemSheet {
     
     get template(){
         return `systems/fi2e-foundry/templates/sheets/${this.item.data.type}-sheet.hbs`; 
-    }
-    
-    /*
-    static get defaultOptions(){
-        return mergeObject(super.defaultOptions, {
-            template: `systems/fi2e-foundry/templates/sheets/${this.item.data.types}-sheet.hbs`,
-            classes: ["fi2e", "sheet", "item"]
-        });
-    }
-    */
-    
+    }    
 }
